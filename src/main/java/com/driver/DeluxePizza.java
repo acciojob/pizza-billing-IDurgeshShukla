a205116a7e539f68dd380970c504beef2b88a8cb
IDurgeshShukla <shukladurgesh4545@gmail.com>
2023-07-14 13:46:10 +0530

    removed space

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,10 +5,7 @@ public class DeluxePizza extends Pizza {
     int price ;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-//        if(isVeg) this.price = 300;
-//        else this.price = 400;
         addExtraCheese();
         addExtraToppings();
-       // base = getPrice();
     }
 }
